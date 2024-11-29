5675e3a2074f70cf0f371769cd268cac2490eaea
Yash Kukrecha <ykukrecha@gmail.com>
2024-11-29 15:03:10 -0600

    tried setting up postgresql database, difficulty connecting

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const getRoutes = require('./routes/getRoutes');
-const postRoutes = require('./routes/postRoutes');
+const express = require("express");
+const cors = require("cors");
+const pool = require("./config/db");
+const getRoutes = require("./routes/getRoutes");
+const postRoutes = require("./routes/postRoutes");
 
 const app = express();
-const PORT = 5000; // Set your desired port
+const PORT = process.env.PORT; // Set your desired port
 
 // Middleware
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 
 // Routes
-app.use('/get', getRoutes);
-app.use('/post', postRoutes);
+app.use("/get", getRoutes);
+app.use("/post", postRoutes);
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
