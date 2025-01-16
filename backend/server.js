4ab4f880c929377ffb6fde5c5e3a4f5c9ac48be4
Aarushi <aaru.lakhi@gmail.com>
2025-01-16 10:54:18 -0600

    add firebase auth in backend

--- backend/server.js
+++ backend/server.js
@@ -3,15 +3,32 @@ const cors = require("cors");
 const { pool, createUsersTable } = require("./config/db");
 const getRoutes = require("./routes/getRoutes");
 const postRoutes = require("./routes/postRoutes");
+const admin = require("firebase-admin");
 
 const app = express();
 const PORT = process.env.PORT; // Set your desired port
 
 // createUsersTable();
 
+// Firebase Admin Authentication Middleware
+const auth = (req, res, next) => {
+  try {
+    const tokenId = req.get("Authorization").split("Bearer ")[1];
+    admin.auth().verifyIdToken(tokenId)
+      .then((decoded) => {
+        req.token = decoded;
+        next();
+      })
+      .catch((error) => res.status(401).send(error));
+  } catch (error) {
+    res.status(400).send("Invalid token");
+  }
+};
+
 // Middleware
 app.use(cors());
 app.use(express.json());
+app.use(auth());
 
 // Routes
 app.use("/get", getRoutes);
